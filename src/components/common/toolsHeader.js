dd3c76b8e9d4275e6533d85127ee02b735a835ab
yangxiaoqiang <18109273442@163.com>
2019-04-11 23:17:50 +0800

    切换组件循环报错解决

--- src/components/common/toolsHeader.js
+++ src/components/common/toolsHeader.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
-import {PageHeader} from 'antd'
-import Tools from './Tools'
-import {tools} from '../../data/data'
+import {PageHeader,Icon} from 'antd'
 
 export default class ToolsHeader extends Component {
   render() {
+    const extra  = this.props.extra && this.props.extra
+    const title = this.props.title && this.props.title
+    const routes = this.props.breadcrumb ? this.props.breadcrumb : []
+    const backIcon = this.props.backIcon  && <Icon type="arrow-left"></Icon>
     return (
-      <PageHeader extra={<Tools tools={tools}  />} className="page-header">
-         
+      <PageHeader 
+        extra={extra}  
+        title={title}  
+        breadcrumb={{ routes }}  
+        backIcon={backIcon} 
+        className={['page-header',this.props.className].join(' ')}>
+        {this.props.children}
       </PageHeader>
     )
   }
--- src/pages/wdwp/index.js
+++ src/pages/wdwp/index.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react'
+import { Input,Icon } from 'antd'
 import AsideMenu from '@components/common/menu'
 import ToolsHeader from '@components/common/toolsHeader'
-import {menu} from '@src/data/data'
+import {menu,tools} from '@src/data/data'
+import Tools from '@components/common/Tools'
+
 
 export default class Wdwp extends Component {
+  constructor(props){
+     super(props)
+     this.state={
+      listStyle:'cell'
+     }
+  }
+  changeIcon(){
+     let style = this.state.listStyle
+     if(style === 'cell'){
+      style = 'list' 
+     }else{
+      style = 'cell' 
+     }
+     this.setState({
+       listStyle:style
+     })
+  }
   render() {
+    const routes =[
+      {
+        path:'/', 
+        breadcrumbName:'首页'
+      },
+      {
+        path:'/wdwp', 
+        breadcrumbName:'我的网盘',
+      }
+    ]
     return (
       <main className="page-main">
-        <AsideMenu menu={menu}/>
+        <AsideMenu menu={menu} />
         <div className="page-inner">
-            <ToolsHeader />
+            <ToolsHeader extra={<Tools tools={tools}  />}>
+              <div className="list-icon" onClick={()=>this.changeIcon()}>
+                 {this.state.listStyle === 'cell' ? <Icon type="bars"></Icon> : <Icon type="appstore"></Icon>}
+              </div>  
+            </ToolsHeader>
+            <ToolsHeader breadcrumb={ routes }  backIcon={true}  className="page-header-bread">
+               <Input.Search />
+            </ToolsHeader>
         </div>
       </main>
     )
